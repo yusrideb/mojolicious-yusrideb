068563cd33ce7d76703cb296aad4ec7d8b9b12d4
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-03-21 07:58:52 +0800

    Update main.pl

--- main.pl
+++ main.pl
@@ -3,12 +3,17 @@ use Mojolicious::Lite;
 
 get '/' => sub {
   my $c = shift;
-  $c->render(text => 'Perl running on App Engine Flexible!');
+  $c->render(text => 'Mojolicious Perl running on App Engine Flexible!');
 };
 
 get '/about' => sub {
   my $c = shift;
   $c->render(text => 'This apps is example Mojolicious Apps');
+};
+
+get '/page1' => sub {
+  my $c = shift;
+  $c->render(text => 'Page1 ');
 };
 
 app->start;
