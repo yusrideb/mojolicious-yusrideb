45ce7f5ef0a9211a00dd5b25f7af6e2cd47a6819
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-03-21 14:54:58 +0800

    Update main.pl & add template library - Route "/" for render template from file "template/index.html.ep" - Add Directory   * "public" to store template library   * "templates" to store Mojolicious Template

--- main.pl
+++ main.pl
@@ -3,17 +3,8 @@ use Mojolicious::Lite;
 
 get '/' => sub {
   my $c = shift;
-  $c->render(text => 'Mojolicious Perl running on App Engine Flexible!');
-};
-
-get '/about' => sub {
-  my $c = shift;
-  $c->render(text => 'This apps is example Mojolicious Apps');
-};
-
-get '/page1' => sub {
-  my $c = shift;
-  $c->render(text => 'Page1 ');
+  $c->render(template => 'index');
 };
 
 app->start;
+
