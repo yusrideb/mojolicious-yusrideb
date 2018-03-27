52bc45988e097e106ba673cfed1d4859df99f2bb
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-03-27 22:02:06 +0800

    Update module "Yusrideb.pm"

--- lib/Yusrideb.pm
+++ lib/Yusrideb.pm
@@ -32,20 +32,20 @@ sub startup {
 #    }
 #  });
   
-  $self->hook(after_dispatch => sub {
-    my $c = shift;
-    my $request_url = $c->req->url->to_abs;
-    if ($request_url->scheme eq 'http') {
-      $request_url->scheme('https');
-      $request_url->host($domain);
-      $self->app->log->info($domain);
-      my $u = $request_url->to_string;
-      $self->app->log->info($u);
-      $u =~ s/\:([\d]+)//g;
-      $self->app->log->info($u);
-      $c->redirect_to($u);
-    }
-  });
+#  $self->hook(after_dispatch => sub {
+#    my $c = shift;
+#    my $request_url = $c->req->url->to_abs;
+#    if ($request_url->scheme eq 'http') {
+#      $request_url->scheme('https');
+#      $request_url->host($domain);
+#      $self->app->log->info($domain);
+#      my $u = $request_url->to_string;
+#      $self->app->log->info($u);
+#      $u =~ s/\:([\d]+)//g;
+#      $self->app->log->info($u);
+#      $c->redirect_to($u);
+#    }
+#  });
   
   $self->hook(after_render => sub {
     my ($c, $output, $format) = @_;
