a2ce08a95807ece9ee9ada88c7d614ca2a4c78a8
Achmad Yusri Afandi <linuxer08@gmail.com>
2018-03-28 16:24:24 +0800

    Update parent module "Yusrideb.pm"

--- lib/Yusrideb.pm
+++ lib/Yusrideb.pm
@@ -10,8 +10,6 @@ our $VERSION = '0.1000';
 sub startup {
   my $self = shift;
   
-  $self->plugin('ACME');
-  
   $self->plugin('Config' => {file => 'myapp.stuff'});
   my $config = $self->config;
   my $domain = $config->{mydomain};
